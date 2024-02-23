47588e34c3ba05bbe2b7d1c017994d8baff63d22
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2024-02-23 19:35:27 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using FormsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FormsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,33 @@
+﻿namespace FormsApp.Models
+{
+    public static class Repository
+    {
+        private static readonly List<Product> _products = new List<Product>();
+
+        private static readonly List<Category> _categories = new List<Category>();
+
+
+        static Repository()
+        {
+
+        }
+
+        public static List<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+
+        }
+
+        public static List<Category> Categories
+        {
+            get
+            {
+                return _categories;
+            }
+
+        }
+    }
+}
